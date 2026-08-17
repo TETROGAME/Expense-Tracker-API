d0259d6ae74a93e25246c75c09ec592d0784fa61
Nikita Timoshenko <nikita.timoshenko.dev@gmail.com>
2026-08-17 16:54:06 +0300

    remove hyphens from package name

---
+++ expense_tracker_api/main.py
@@ -0,0 +1,32 @@
+from datetime import datetime
+
+from expense import Expense, ExpenseCategory, ExpenseManager
+
+
+def main():
+    expense1 = Expense(amount=99)
+    expense2 = Expense(amount=-1)
+    expense3 = Expense(amount=67)
+    expense4 = Expense(amount=4)
+    mgr = ExpenseManager()
+    mgr.add(expense1)
+    mgr.add(expense2)
+    mgr.add(expense3)
+    mgr.add(expense4)
+
+    mgr._print_expenses()
+
+    mgr.delete(0)
+    mgr.delete(-1)
+    print("\n")
+
+    mgr._print_expenses()
+
+    expense5 = Expense(amount=12355)
+    mgr.add(expense5)
+
+    mgr._print_expenses()
+
+
+if __name__ == "__main__":
+    main()
