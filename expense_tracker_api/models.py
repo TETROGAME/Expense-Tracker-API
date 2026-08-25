68e346d0933358644cbed216233af754049dbf8b
Nikita Timoshenko <nikita.timoshenko.dev@gmail.com>
2026-08-25 13:00:20 +0300

    add expense category

--- expense_tracker_api/models.py
+++ expense_tracker_api/models.py
@@ -1,3 +1,5 @@
+from enum import Enum, auto
+
 from sqlalchemy import String
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
@@ -6,9 +8,20 @@ class Base(DeclarativeBase):
     pass
 
 
+class ExpenseCategory(Enum):
+    Groceries = auto()
+    Leisure = auto()
+    Electronics = auto()
+    Utilities = auto()
+    Clothing = auto()
+    Health = auto()
+    Others = auto()
+
+
 class ExpenseORM(Base):
     __tablename__ = "expenses"
 
     id: Mapped[int] = mapped_column(primary_key=True)
     amount: Mapped[float]
+    category: Mapped[ExpenseCategory]
     description: Mapped[str] = mapped_column(String(256))
