388e2397332d8ba5b4a654b4421f571a67063dc3
Nikita Timoshenko <nikita.timoshenko.dev@gmail.com>
2026-08-25 12:58:03 +0300

    add base ExpenseORM table description

--- expense_tracker_api/models.py
+++ expense_tracker_api/models.py
@@ -1,9 +1,14 @@
-from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy import String
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 
 class Base(DeclarativeBase):
     pass
 
 
-class Expense(Base):
+class ExpenseORM(Base):
     __tablename__ = "expenses"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    amount: Mapped[float]
+    description: Mapped[str] = mapped_column(String(256))
