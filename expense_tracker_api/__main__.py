36f396f67fd5ac25dc5dbaad3345d6bc7bedd347
Nikita Timoshenko <nikita.timoshenko.dev@gmail.com>
2026-08-30 14:53:42 +0300

    add fastapi app initialization

--- expense_tracker_api/__main__.py
+++ expense_tracker_api/__main__.py
@@ -1,29 +1,13 @@
-from expense_tracker_api.expense import Expense, ExpenseManager
+import uvicorn
+from fastapi import FastAPI
+
+app = FastAPI()
 
 
 def main():
-    expense1 = Expense(amount=99)
-    expense2 = Expense(amount=-1)
-    expense3 = Expense(amount=67)
-    expense4 = Expense(amount=4)
-    mgr = ExpenseManager()
-    mgr.add(expense1)
-    mgr.add(expense2)
-    mgr.add(expense3)
-    mgr.add(expense4)
-
-    mgr._print_expenses()
-
-    mgr.delete(0)
-    mgr.delete(-1)
-    print("\n")
-
-    mgr._print_expenses()
-
-    expense5 = Expense(amount=12355)
-    mgr.add(expense5)
-
-    mgr._print_expenses()
+    uvicorn.run(
+        "expense_tracker_api.__main__:app", host="0.0.0.0", port=8000, reload=True
+    )
 
 
 if __name__ == "__main__":
